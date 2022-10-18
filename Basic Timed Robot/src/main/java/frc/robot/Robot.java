11d093c706dc272cfc3120953cc7a84d85bf0a8c
Nave <35505431+nullphia@users.noreply.github.com>
2022-10-18 08:56:15 -0400

    Joystick controls and basic 5 second autonomous

--- Basic Timed Robot/src/main/java/frc/robot/Robot.java
+++ Basic Timed Robot/src/main/java/frc/robot/Robot.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,23 +20,61 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+  private Spark leftMotor1 = new Spark(0); //initialize leftMotor1 as a Spark Max motor controller (controller #0)
+  private Spark leftMotor2 = new Spark(1); //initialize leftMotor2 as a Spark Max motor controller (controller #1)
+  private Spark rightMotor1 = new Spark(2); //initialize rightMotor1 as a Spark Max motor controller (controller #2)
+  private Spark rightMotor2 = new Spark(3); //initialize rightMotor2 as a Spark Max motor controller (controller #3)
+
+  private Joystick joy1 = new Joystick(0);
+
+  private double startTime;
+
   @Override
-  public void robotInit() {}
+  public void robotInit() {} //initialize function
 
   @Override
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+  startTime = Timer.getFPGATimestamp();
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    double time = Timer.getFPGATimestamp();
+    System.out.println(time - startTime);
+
+    if (time - startTime < 3) {
+      leftMotor1.set(0.6);
+      leftMotor2.set(0.6);
+      rightMotor1.set(-0.6);
+      rightMotor2.set(-0.6);
+    } else {
+      leftMotor1.set(0);
+      leftMotor2.set(0);
+      rightMotor1.set(0);
+      rightMotor2.set(0);
+    }
+  }
 
   @Override
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    double speed = -joy1.getRawAxis(1) * 0.6;
+    double turn = joy1.getRawAxis(4) * 0.3;
+
+    double left = speed + turn;
+    double right = speed - turn;
+
+    leftMotor1.set(left);
+    leftMotor2.set(left);
+    rightMotor1.set(-right);
+    rightMotor2.set(-right);
+
+  }
 
   @Override
   public void disabledInit() {}
