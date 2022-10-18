2247ca6d0cf4a2fc32ac7c3b15ea5f8cbffb092b
Nave <35505431+nullphia@users.noreply.github.com>
2022-10-18 15:25:49 -0400

    Arcade Drive & Simple Auto

--- Basic Timed Robot/src/main/java/frc/robot/Robot.java
+++ Basic Timed Robot/src/main/java/frc/robot/Robot.java
@@ -4,15 +4,19 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-//import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import java.util.Set;
 
-//import edu.wpi.first.wpilibj.Joystick; //generic joystick library
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-//import edu.wpi.first.wpilibj.motorcontrol.Spark; //spark controller
-//import edu.wpi.first.wpilibj.Timer; //autonomous timer
+import edu.wpi.first.wpilibj.Timer; //autonomous timer
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -26,47 +30,48 @@ public class Robot extends TimedRobot {
    * initialization code.
    */
 
-  private VictorSPX leftMotor1 = new VictorSPX(1); //initialize leftMotor1 as a TalonSRX motor controller (controller #0)
-  private VictorSPX leftMotor2 = new VictorSPX(2); //initialize leftMotor2 as a TalonSRX motor controller (controller #1)
-  private VictorSPX rightMotor1 = new VictorSPX(3); //initialize rightMotor1 as a TalonSRX motor controller (controller #2)
-  private VictorSPX rightMotor2 = new VictorSPX(4); //initialize rightMotor2 as a TalonSRX motor controller (controller #3)
+  private WPI_VictorSPX leftMotor1 = new WPI_VictorSPX(1); //initialize leftMotor1 as a TalonSRX motor controller (controller #0)
+  private WPI_VictorSPX leftMotor2 = new WPI_VictorSPX(2); //initialize leftMotor2 as a TalonSRX motor controller (controller #1)
+  private WPI_VictorSPX rightMotor1 = new WPI_VictorSPX(3); //initialize rightMotor1 as a TalonSRX motor controller (controller #2)
+  private WPI_VictorSPX rightMotor2 = new WPI_VictorSPX(4); //initialize rightMotor2 as a TalonSRX motor controller (controller #3)
 
+  private MotorControllerGroup right = new MotorControllerGroup(rightMotor1, rightMotor2);
+  private MotorControllerGroup left = new MotorControllerGroup(leftMotor1, leftMotor2);
+
+
+  private DifferentialDrive drivetrain = new DifferentialDrive(left, right);
   //private Joystick joy1 = new Joystick(0); //generic joystick controller
-  private XboxController joy1 = new XboxController(0);
+  private XboxController joy1 = new XboxController(1);
 
-//  private double startTime; // used in autonomous timing
+  public double startTime; // used in autonomous timing
 
   @Override
-  public void robotInit() {} //initialize function
+  public void robotInit() {
+    rightMotor1.setInverted(true); 
+    rightMotor2.setInverted(true);
+  } //initialize function
 
   @Override
   public void robotPeriodic() {}
 
   @Override
   public void autonomousInit() {
-//startTime = Timer.getFPGATimestamp();
+    startTime = Timer.getFPGATimestamp();
   }
 
   @Override
   public void autonomousPeriodic() {
-    /*
-    double time = Timer.getFPGATimestamp();
-    System.out.println(time - startTime);
-    double autonSpeed = 0.2; //speed of motors -1 through 1 (min - max) during autonomous peroid
+    
+    System.out.println(Timer.getFPGATimestamp());
 
-    if (time - startTime < 3) {
-      leftMotor1.set(autonSpeed); //set motors to spin at autonSpeed during autonoums peroid
-      leftMotor2.set(autonSpeed);
-      rightMotor1.set(-autonSpeed);
-      rightMotor2.set(-autonSpeed);
-      leftMotor1.set(Mode, demand);
-    } else {
-      leftMotor1.set(0);
-      leftMotor2.set(0);
-      rightMotor1.set(0);
-      rightMotor2.set(0);
+    double time = Timer.getFPGATimestamp();
+
+    if ((startTime - time) < 3) {
+      drivetrain.arcadeDrive(-0.5, 0);
+    } 
+    else {
+      drivetrain.arcadeDrive(0,0);
     }
-    */
   }
 
   @Override
@@ -74,17 +79,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    double speed = joy1.getLeftX() * 0.6;
-    double turn = joy1.getRightY() * 0.3;
-
-    double left = speed + turn;
-    double right = speed - turn;
-
-    leftMotor1.set(ControlMode.PercentOutput, 1);
-    leftMotor2.set(ControlMode.PercentOutput, 1);
-    rightMotor1.set(ControlMode.PercentOutput, -1);
-    rightMotor2.set(ControlMode.PercentOutput, -1);
-
+    drivetrain.arcadeDrive(joy1.getLeftY() * 0.8, joy1.getRightX() * 0.6); //controller axis mapped to drive motors, with sensitivity values
   }
 
   @Override
